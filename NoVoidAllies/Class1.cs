1c274f31618a0b3fe162f2040e7ed483adc9bb98
Moffein <22742925+Moffein@users.noreply.github.com>
2022-03-02 20:15:43 -0700

    Add project files.

---
+++ NoVoidAllies/Class1.cs
@@ -0,0 +1,27 @@
+﻿using BepInEx;
+using MonoMod.Cil;
+using RoR2;
+using System;
+
+namespace NoVoidAllies
+{
+    [BepInPlugin("com.Moffein.NoVoidAllies", "No Void Allies", "1.0.0")]
+    public class NoVoidAllies : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            IL.EntityStates.VoidInfestor.Infest.OnEnter += (il) =>
+            {
+                ILCursor c = new ILCursor(il);
+                c.GotoNext(
+                     x => x.MatchCallvirt<BullseyeSearch>("RefreshCandidates")
+                    );
+                c.EmitDelegate<Func<BullseyeSearch, BullseyeSearch>>(search =>
+                {
+                    search.teamMaskFilter.RemoveTeam(TeamIndex.Player);
+                    return search;
+                });
+            };
+        }
+    }
+}
