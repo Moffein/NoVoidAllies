4df4345f3e2af50bd2dd70ff72908b424a5cd6d5
Moffein <22742925+Moffein@users.noreply.github.com>
2022-03-04 00:10:22 -0700

    Added an extra check.

--- NoVoidAllies/Class1.cs
+++ NoVoidAllies/Class1.cs
@@ -1,11 +1,12 @@
 ﻿using BepInEx;
+using Mono.Cecil.Cil;
 using MonoMod.Cil;
 using RoR2;
 using System;
 
 namespace NoVoidAllies
 {
-    [BepInPlugin("com.Moffein.NoVoidAllies", "No Void Allies", "1.0.0")]
+    [BepInPlugin("com.Moffein.NoVoidAllies", "No Void Allies", "1.0.1")]
     public class NoVoidAllies : BaseUnityPlugin
     {
         public void Awake()
@@ -20,6 +21,19 @@ namespace NoVoidAllies
                 {
                     search.teamMaskFilter.RemoveTeam(TeamIndex.Player);
                     return search;
+                });
+            };
+
+            IL.EntityStates.VoidInfestor.Infest.FixedUpdate += (il) =>
+            {
+                ILCursor c = new ILCursor(il);
+                c.GotoNext(MoveType.After,
+                     x => x.MatchCallvirt<CharacterBody>("get_isPlayerControlled")
+                    );
+                c.Emit(OpCodes.Ldloc_3);
+                c.EmitDelegate<Func<bool, CharacterBody, bool>>((playerControlled, body) =>
+                {
+                    return playerControlled || (body.teamComponent && body.teamComponent.teamIndex == TeamIndex.Player);
                 });
             };
         }
